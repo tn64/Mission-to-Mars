5837a672c7d623bcf1bb7a815ac43a45ad02e7d6
tn64 <77516099+tn64@users.noreply.github.com>
2021-05-17 10:32:56 -0600

    Add html file

---
+++ app.py
@@ -0,0 +1,29 @@
+#Import Dependencies
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+#Set up Flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Set up app routes - home page
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+# Set up scrape route and use scraping.py file
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return redirect('/', code=302)
+
+#Tell flask to run
+if __name__ == "__main__":
+   app.run()
--- scraping.py
+++ scraping.py
@@ -1,12 +1,30 @@
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
-#Set executable path and url
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    # Initiate headless driver for deployment
+    #Set executable path and url
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    
+    news_title, news_paragraph = mars_news(browser)
+    
+    # Run all scraping functions and store results in a dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 #SCRAPE MARS NEWS 
 def mars_news(browser):
@@ -31,21 +49,8 @@ def mars_news(browser):
     except AttributeError:
         return None, None
     
-    slide_elem = news_soup.select_one('div.list_text')
-
-    #Begin Scrape
-    slide_elem.find('div', class_='content_title')
-
-    # Use the parent element to find the first `a` tag and save it as `news_title`
-    news_title = slide_elem.find('div', class_='content_title').get_text()
-    news_title
-
-    # Use the parent element to find the paragraph text
-    news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-    news_p
-    
     return news_title, news_p
-
+    
 # ### JPL SPACE IMAGES FEATURED IMAGE
 def featured_image(browser):
     # Visit URL
@@ -69,7 +74,7 @@ def featured_image(browser):
         return None
     
     # Use the base URL to create an absolute URL
-    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
     
     return img_url
     
@@ -78,7 +83,7 @@ def mars_facts():
     # Add try/except for error handling
     try:
         # Use 'read_html' to scrape the facts table into a dataframe
-        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
 
     except BaseException:
         return None
@@ -88,7 +93,9 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html()(classes="table table-striped")
 
-browser.quit()
+if __name__ == "__main__":
 
+    # If running as script, print scraped data
+    print(scrape_all())
